dfc24e6c8f1763f3c59b7a027089ee5685931252
Joshua Taguicana <joshtaguicana@gmail.com>
2019-06-10 12:17:24 -0400

    charts nested routes added

--- client/src/Components/PickGraph/PickGraph.js
+++ client/src/Components/PickGraph/PickGraph.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import './PickGraph.scss';
+import { Link } from 'react-router-dom';
 
 export class PickGraph extends Component {
     render() {
         const { name_enus } = this.props.itemHistory[6];
 
+        console.log(this.props.match.url)
+
         return (
             <div>
                 <h3>Pick a Graph for {name_enus}</h3>
                 <div className="buttonContainer">
-                    <button className="buttonContainer__btn" onClick={this.props.price}>Price Information</button>
-                    <button className="buttonContainer__btn" onClick={this.props.quant}>Quantity Information</button>
-                    <button className="buttonContainer__btn" onClick={this.props.priceQuant}>Price Quanitity Comparison</button>
+                    <Link 
+                        to={`${this.props.match.url}/priceChart`}
+                        className="buttonContainer__btn">
+                            Price Information
+                    </Link>
+                    <Link 
+                        to={`${this.props.match.url}/quantChart`}
+                        className="buttonContainer__btn">
+                            Quantity Information
+                    </Link>
+                    <Link 
+                        to={`${this.props.match.url}/priceQuantChart`}
+                        className="buttonContainer__btn">
+                            Price Quanitity Comparison
+                    </Link>
                 </div>
             </div>
         )
