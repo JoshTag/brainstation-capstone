b44db404e6e8bff6fcf5c667af2d12675951bfb1
Joshua Taguicana <joshtaguicana@gmail.com>
2019-07-19 23:07:26 -0400

    updated styling

--- client/src/Components/DashboardHome/DashboardHome.js
+++ client/src/Components/DashboardHome/DashboardHome.js
@@ -39,19 +39,19 @@ export class TopItems extends Component {
                             <h3>Goblin Glider Kit</h3>
                             <p>924 g</p>
                         </Link>
-                        <Link to={`${this.props.url}/details/117`}className="dashboardCards__cards--center">
+                        <Link to={`${this.props.url}/details/117`} className="dashboardCards__cards--center">
                             <h3>Tough Jerky</h3>
                             <p>198 g</p>
                         </Link>
-                        <Link to={`${this.props.url}/details/109118`}className="dashboardCards__cards--center">
+                        <Link to={`${this.props.url}/details/109118`} className="dashboardCards__cards--center">
                             <h3>Blackrock Ore</h3>
                             <p>266 g</p>
                         </Link>
-                        <Link to={`${this.props.url}/details/152508`}className="dashboardCards__cards--center">
+                        <Link to={`${this.props.url}/details/152508`} className="dashboardCards__cards--center">
                             <h3>Winter's Kiss</h3>
                             <p>512 g</p>
                         </Link>
-                        <Link to={`${this.props.url}/details/152507`}className="dashboardCards__cards--center">
+                        <Link to={`${this.props.url}/details/152507`} className="dashboardCards__cards--center">
                             <h3>Blood-Stained Bone</h3>
                             <p>473 g</p>
                         </Link>
@@ -64,19 +64,19 @@ export class TopItems extends Component {
                             <h3>Akunda's Bite</h3>
                             <p>1,360 g</p>
                         </Link>
-                        <Link to={`${this.props.url}/details/152512`}className="dashboardCards__cards--center">
+                        <Link to={`${this.props.url}/details/152512`} className="dashboardCards__cards--center">
                             <h3>Monelite Ore</h3>
                             <p>725 g</p>
                         </Link>
-                        <Link to={`${this.props.url}/details/152510`}className="dashboardCards__cards--center">
+                        <Link to={`${this.props.url}/details/152510`} className="dashboardCards__cards--center">
                             <h3>Anchor Weed</h3>
                             <p>2,725 g</p>
                         </Link>
-                        <Link to={`${this.props.url}/details/152508`}className="dashboardCards__cards--center">
+                        <Link to={`${this.props.url}/details/152508`} className="dashboardCards__cards--center">
                             <h3>Winter's Kiss</h3>
                             <p>512 g</p>
                         </Link>
-                        <Link to={`${this.props.url}/details/152513`}className="dashboardCards__cards--center">
+                        <Link to={`${this.props.url}/details/152513`} className="dashboardCards__cards--center">
                             <h3>Platinum Ore</h3>
                             <p>2,213 g</p>
                         </Link>
@@ -89,19 +89,19 @@ export class TopItems extends Component {
                             <h3>Anchor Weed</h3>
                             <p>2,725 g</p>
                         </Link>
-                        <Link to={`${this.props.url}/details/152513`}className="dashboardCards__cards--center">
+                        <Link to={`${this.props.url}/details/152513`} className="dashboardCards__cards--center">
                             <h3>Platinum Ore</h3>
                             <p>2,213 g</p>
                         </Link>
-                        <Link to={`${this.props.url}/details/163223`}className="dashboardCards__cards--center">
+                        <Link to={`${this.props.url}/details/163223`} className="dashboardCards__cards--center">
                             <h3>Battle Potion of Agility</h3>
                             <p>10,217 g</p>
                         </Link>
-                        <Link to={`${this.props.url}/details/163222`}className="dashboardCards__cards--center">
+                        <Link to={`${this.props.url}/details/163222`} className="dashboardCards__cards--center">
                             <h3>Battle Potion of Intellect</h3>
                             <p>9,977 g</p>
                         </Link>
-                        <Link to={`${this.props.url}/details/163224`}className="dashboardCards__cards--center">
+                        <Link to={`${this.props.url}/details/163224`} className="dashboardCards__cards--center">
                             <h3>Battle Potion of Strength</h3>
                             <p>6,157 g</p>
                         </Link>
